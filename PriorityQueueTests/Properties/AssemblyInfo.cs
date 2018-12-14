a4b4447c0b5db41610ae7bb3ff3daf4fe169b60e
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2018-12-14 11:22:55 +0100

    Fixed references to TestProject

---
+++ PriorityQueueTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PriorityQueueTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PriorityQueueTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eb46bde7-4a51-43ef-89b0-1db89909e1f0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PriorityQueueTests/SkewHeapTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PriorityQueue;
+
+namespace PriorityQueueTests
+{
+    [TestClass]
+    public class SkewHeapTests : MergeablePriorityQueueTests<SkewHeap<int>>
+    {
+        public override SkewHeap<int> CreateMergeableInstance() => new SkewHeap<int>();
+    }
+}
