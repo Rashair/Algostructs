5c37da9e9db684995d9c49e6a8592c2e3b384db4
Rashair <przemyslaw.wk@gmail.com>
2023-12-16 16:29:18 +0100

    Add extra performance test

--- Strings.Tests/DsStringBuilderTests/ReplaceTests.cs
+++ Strings.Tests/DsStringBuilderTests/ReplaceTests.cs
@@ -80,7 +80,7 @@ public class ReplaceTests
     [TestCase(25_000, 5_000)]
     [TestCase(10_000, 1_000)]
     [TestCase(1_000, 10)]
-    public void GivenEnormousNumberOfStrings_ThenTheReplaceIsFast(int numberOfReplacements,
+    public void GivenEnormousNumberOfStrings_ThenTheReplaceIsFast(int numberOfStrings,
         int timeoutMs)
     {
         // Arrange
@@ -89,7 +89,7 @@ public class ReplaceTests
         // Act
         var ct = TestsHelper.CreateCancellationToken(timeoutMs);
         const string guid = "F32BD441-DF16-44C0-AC0D-6DBABE95260D";
-        for (int i = 0; i < numberOfReplacements; ++i)
+        for (int i = 0; i < numberOfStrings; ++i)
         {
             sb.Append(guid);
             ct.ThrowIfCancellationRequested();
@@ -98,6 +98,31 @@ public class ReplaceTests
         var result = sb.ToString();
 
         // Assert
-        Assert.That(result, Has.Length.EqualTo(numberOfReplacements));
+        Assert.That(result, Has.Length.EqualTo(numberOfStrings));
+    }
+
+
+    [TestCase(50_000, 10_000)]
+    [TestCase(25_000, 5_000)]
+    [TestCase(10_000, 1_000)]
+    [TestCase(1_000, 10)]
+    public void GivenEnormousNumberOfReplacements_ThenTheReplaceIsFast(int numberOfReplacements,
+        int timeoutMs)
+    {
+        // Arrange
+        var sb = new DsStringBuilder(new('a', numberOfReplacements));
+
+        // Act
+        var ct = TestsHelper.CreateCancellationToken(timeoutMs);
+        const string replacement = "23";
+        for (int i = 0; i < numberOfReplacements; ++i)
+        {
+            ct.ThrowIfCancellationRequested();
+            sb.Replace("a", replacement);
+        }
+        var result = sb.ToString();
+
+        // Assert
+        Assert.That(result, Has.Length.EqualTo(numberOfReplacements * replacement.Length));
     }
 }
