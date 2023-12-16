e0a996734b5a1d508cedd7818f760b44e2183bd6
Rashair <przemyslaw.wk@gmail.com>
2023-12-16 13:23:42 +0100

    Add DsStringBuilder along with proxy implementation

---
+++ Strings/DsStringBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace Strings;
+
+public class DsStringBuilder
+{
+    private readonly StringBuilder _builder;
+
+    public DsStringBuilder()
+    {
+        _builder = new StringBuilder();
+    }
+
+    public DsStringBuilder(string str)
+    {
+        _builder = new StringBuilder(str);
+    }
+
+    public void Append(string str)
+    {
+        _builder.Append(str);
+    }
+
+    public void Insert(int index, string str)
+    {
+        _builder.Insert(index, str);
+    }
+
+    public void Remove(int startIndex, int length)
+    {
+        _builder.Remove(startIndex, length);
+    }
+
+    public void Replace(string pattern, string replacement)
+    {
+        _builder.Replace(pattern, replacement);
+    }
+
+    public void Clear()
+    {
+        _builder.Clear();
+    }
+
+    public int Length => _builder.Length;
+
+    public override string ToString()
+    {
+        return _builder.ToString();
+    }
+}
