2b5be488f264ff1a101771750f03560afcd538ac
Rashair <przemyslaw.wk@gmail.com>
2023-12-18 20:22:07 +0100

    Fix limits

--- Sorting.Tests/InsertionSortTests.cs
+++ Sorting.Tests/InsertionSortTests.cs
@@ -9,7 +9,7 @@ public class InsertionSortTests : SortingTests
         return new InsertionSort<int>(c => c);
     }
 
-    [TestCase(10_000, 400)]
+    [TestCase(10_000, 500)]
     [TestCase(25_000, 2_000)]
     [TestCase(50_000, 10_000)]
     public void Performance(int size, int timeLimit)
@@ -17,7 +17,7 @@ public class InsertionSortTests : SortingTests
         base.Performance(size, timeLimit);
     }
 
-    [TestCase(10_000, 300)]
+    [TestCase(10_000, 500)]
     [TestCase(25_000, 2_000)]
     [TestCase(50_000, 10_000)]
     public void PerformanceSmallRange(int size, int timeLimit)
