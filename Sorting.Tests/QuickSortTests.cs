89a066216308963905f0c5c9ec3a3985958a0bab
Rashair <przemyslaw.wk@gmail.com>
2023-12-18 19:48:05 +0100

    Add quick sort

--- Sorting.Tests/QuickSortTests.cs
+++ Sorting.Tests/QuickSortTests.cs
@@ -1,25 +1,25 @@
 ﻿namespace Sorting.Tests;
 
 [TestFixture]
-[TestOf(typeof(InsertionSort<>))]
-public class InsertionSortTests : SortingTests
+[TestOf(typeof(QuickSort<>))]
+public class QuickSortTests : SortingTests
 {
     protected override ISort<int> InitSort()
     {
-        return new InsertionSort<int>(c => c);
+        return new QuickSort<int>(c => c);
     }
 
-    [TestCase(10_000, 400)]
-    [TestCase(25_000, 2_000)]
-    [TestCase(75_000, 15_000)]
+    [TestCase(10_000, 50)]
+    [TestCase(100_000, 100)]
+    [TestCase(10_000_000, 4_500)]
     public void Performance(int size, int timeLimit)
     {
         base.Performance(size, timeLimit);
     }
 
-    [TestCase(10_000, 300)]
-    [TestCase(25_000, 2_000)]
-    [TestCase(75_000, 15_000)]
+    [TestCase(10_000, 50)]
+    [TestCase(100_000, 200)]
+    [TestCase(500_000, 5_000)]
     public void PerformanceSmallRange(int size, int timeLimit)
     {
         base.Performance(size, timeLimit, 100);
---
+++ Sorting/QuickSort.cs
@@ -0,0 +1,93 @@
+﻿namespace Sorting;
+
+public class QuickSort<T> : ISort<T>
+{
+    private readonly Func<T, int> _keyConverter;
+
+    public QuickSort(Func<T, int> keyConverter)
+    {
+        _keyConverter = keyConverter;
+    }
+
+    public IList<T> Apply(IList<T> values)
+    {
+        InternalSort(values, 0, values.Count - 1);
+
+        return values;
+    }
+
+    private void InternalSort(IList<T> values, int low, int high)
+    {
+        while (low < high)
+        {
+            int medianIndex = MedianOf3Pivots(values, low, (low + high) / 2, high);
+            Swap(values, medianIndex, high);
+            int pivotIndex = Partition(values, low, high);
+
+            if (pivotIndex - low < high - pivotIndex)
+            {
+                InternalSort(values, low, pivotIndex - 1);
+                low = pivotIndex + 1;
+            }
+            else
+            {
+                InternalSort(values, pivotIndex + 1, high);
+                high = pivotIndex - 1;
+            }
+        }
+    }
+
+    private int MedianOf3Pivots(IList<T> values, int a, int b, int c)
+    {
+        var valA = _keyConverter(values[a]);
+        var valB = _keyConverter(values[b]);
+        var valC = _keyConverter(values[c]);
+
+        if (valA < valB)
+        {
+            if (valB < valC)
+            {
+                return b;
+            }
+
+            return valA < valC ? c : a;
+        }
+
+        // valB <= valA
+        if (valA < valC)
+        {
+            return a;
+        }
+
+        return valB < valC ? c : b;
+    }
+
+    private int Partition(IList<T> values, int low, int high)
+    {
+        var pivot = _keyConverter(values[high]);
+        int i = low - 1;
+        for (int j = low; j < high; ++j)
+        {
+            if (_keyConverter(values[j]) >= pivot)
+            {
+                continue;
+            }
+
+            ++i;
+            if(i != j)
+            {
+                Swap(values, i, j);
+            }
+        }
+
+        // Swap pivot value in its rightful place
+        Swap(values, i + 1, high);
+
+        return i + 1;
+    }
+
+    private static void Swap(IList<T> values, int a, int b)
+    {
+        (values[a], values[b]) = (values[b], values[a]);
+    }
+}
