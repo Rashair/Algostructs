368f41ad37698e842b9768c02d1797344d6fd5ff
Przemyslaw Wiczolek <przemyslaw.wk@gmail.com>
2023-12-26 14:45:55 +0100

    Add stacks project

---
+++ Stacks/MultiStack.cs
@@ -0,0 +1,41 @@
+﻿namespace Stacks;
+
+public class MultiStack<T> : IStack<T>
+{
+    private readonly T[] _values;
+    private readonly MyStack[] _stacks;
+
+    public MultiStack(int stacksNum)
+    {
+        _values = Array.Empty<T>();
+        _stacks = new MyStack[stacksNum];
+    }
+
+    public void Push(T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Pop()
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Peek()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsEmpty()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private class MyStack
+    {
+        public int First { get; set; }
+        public int Size { get; set; }
+        public int Capacity { get; set; }
+        public int Last => First - Size + 1;
+    }
+}
