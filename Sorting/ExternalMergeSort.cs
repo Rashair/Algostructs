71fcafd1af2ec5a65285957a73476d3b34482b64
Rashair <przemyslaw.wk@gmail.com>
2024-01-27 13:52:57 +0100

    Add external merge sort interface

---
+++ Sorting/ExternalMergeSort.cs
@@ -0,0 +1,17 @@
+﻿namespace Sorting;
+
+public interface IExternalSort
+{
+    void Apply(string filePath);
+}
+
+public class ExternalMergeSort : IExternalSort
+{
+    public void Apply(string filePath)
+    {
+        // throw new NotImplementedException();
+        using var sr = new StreamReader(filePath);
+        sr.Read();
+
+    }
+}
