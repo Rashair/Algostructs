e96b0ac31559deddda817a95f2170990e8d41adb
Rashair <przemyslaw.wk@gmail.com>
2024-02-04 17:38:04 +0100

    Add EventProcessor

---
+++ Messaging/EventObserver.cs
@@ -0,0 +1,33 @@
+﻿namespace Messaging;
+
+public class EventObserver<TEvent> : IObserver<TEvent>
+{
+    private readonly Random _random;
+    private readonly int _number;
+    private readonly string _name;
+    private readonly TextWriter _output;
+
+    public EventObserver(int? number = null, TextWriter? output = null)
+    {
+        _random = new();
+        _number = number ?? Random.Shared.Next(100);
+        _name = $"[O{number}]";
+        _output = output ?? Console.Out;
+    }
+
+    public void OnCompleted()
+    {
+        _output.WriteLine($"{_name} Completed!");
+    }
+
+    public void OnError(Exception error)
+    {
+        _output.WriteLine($"{_name} Error: {error.Message}");
+    }
+
+    public void OnNext(TEvent value)
+    {
+        _output.WriteLine($"{_name} Next: {value}");
+        Thread.Sleep(_random.Next(100 / (_number + 1)));
+    }
+}
---
+++ Messaging/EventProcessor.cs
@@ -0,0 +1,51 @@
+﻿namespace Messaging;
+
+public class EventProcessor<TEvent> : IObservable<TEvent>
+{
+    private readonly HashSet<IObserver<TEvent>> _observers;
+    private readonly HashSet<TEvent> _events;
+
+    public EventProcessor()
+    {
+        _observers = [];
+        _events = [];
+    }
+
+    public IDisposable Subscribe(IObserver<TEvent> observer)
+    {
+        if (_observers.Add(observer))
+        {
+            // Provide observer with existing data.
+            foreach (var item in _events)
+            {
+                observer.OnNext(item);
+            }
+        }
+
+        return new Unsubscriber<TEvent>(_observers, observer);
+    }
+
+    public void OnEvent(TEvent @event)
+    {
+        _events.Add(@event);
+        Parallel.ForEach(_observers, o => o.OnNext(@event));
+    }
+
+    public void OnCompleted()
+    {
+        Parallel.ForEach(_observers, o => o.OnCompleted());
+        _observers.Clear();
+    }
+}
+
+internal sealed class Unsubscriber<TEvent> : IDisposable
+{
+    private readonly ISet<IObserver<TEvent>> _observers;
+    private readonly IObserver<TEvent> _observer;
+
+    internal Unsubscriber(
+        ISet<IObserver<TEvent>> observers,
+        IObserver<TEvent> observer) => (_observers, _observer) = (observers, observer);
+
+    public void Dispose() => _observers.Remove(_observer);
+}
