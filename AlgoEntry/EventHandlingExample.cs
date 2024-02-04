8c0b3635af857d5b4ad9c132a55314820ebd245b
Rashair <przemyslaw.wk@gmail.com>
2024-02-04 17:38:12 +0100

    Add examples

---
+++ AlgoEntry/EventHandlingExample.cs
@@ -0,0 +1,23 @@
+﻿using Messaging;
+
+namespace AlgoEntry;
+
+public class EventHandlingExample
+{
+    public static void Play()
+    {
+        var processor = new EventProcessor<int>();
+        for (int i = 0; i < 5; ++i)
+        {
+            var observer = new EventObserver<int>(i);
+            processor.Subscribe(observer);
+        }
+
+        for (int j = 0; j < 100; j++)
+        {
+            processor.OnEvent(j);
+        }
+
+        processor.OnCompleted();
+    }
+}
--- AlgoEntry/Program.cs
+++ AlgoEntry/Program.cs
@@ -4,13 +4,4 @@ using AlgoEntry;
 using Messaging;
 
 
-var processor = new MessageProcessor<int>();
-
-for (int j = 0; j < 1_000; j++)
-{
-    processor.OnMessage(j);
-    await Task.Delay(10);
-}
-
-
-await processor.StopProcessing();
+EventHandlingExample.Play();
