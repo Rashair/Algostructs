f6a1e49c35fac5b6f80616b05d905358beb1f39c
Przemysław Wiczołek <33404585+Rashair@users.noreply.github.com>
2024-03-03 20:33:46 +0100

    Add `ReSpacer` (#12)
    * Add basic working version
    
    * Add working version with memoization
    
    * Add extra test

---
+++ Strings.Tests/ReSpacerTests.cs
@@ -0,0 +1,43 @@
+﻿namespace Strings.Tests;
+
+public class ReSpacerTests
+{
+    [Test]
+    public void BaseCase()
+    {
+        var document = "jesslookedjustliketimherbrother";
+        var dictionary = new HashSet<string>()
+        {
+            "looked",
+            "just",
+            "like",
+            "her",
+            "brother"
+        };
+
+        var spacer = new ReSpacer(dictionary, document);
+
+        // Act
+       var res = spacer.ReSpace();
+
+        Assert.That(res, Is.EqualTo("jess looked just like tim her brother"));
+    }
+
+    [Test]
+    public void OtherCase()
+    {
+        var document = "jesstephen";
+        var dictionary = new HashSet<string>()
+        {
+            "jess",
+            "stephen",
+        };
+
+        var spacer = new ReSpacer(dictionary, document);
+
+        // Act
+        var res = spacer.ReSpace();
+
+        Assert.That(res, Is.EqualTo("jes stephen"));
+    }
+}
---
+++ Strings/ReSpacer.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+
+namespace Strings;
+
+public class ReSpacer
+{
+    private readonly HashSet<string> _dictionary;
+    private readonly string _document;
+
+    private readonly ParseResult?[] _memory;
+
+    public ReSpacer(HashSet<string> dictionary, string document)
+    {
+        _dictionary = dictionary;
+        _document = document;
+
+        _memory = new ParseResult[document.Length];
+    }
+
+    public string? ReSpace()
+    {
+        if (string.IsNullOrEmpty(_document))
+        {
+            return _document;
+        }
+
+        var parseResult = ReSpace(0);
+
+        return parseResult.Document;
+    }
+
+    private ParseResult ReSpace(int start)
+    {
+        if (start >= _document.Length)
+        {
+            return new(0, "");
+        }
+
+        if (_memory[start] != null)
+        {
+            return _memory[start]!;
+        }
+
+        var bestUnrecognisedChars = int.MaxValue;
+        var bestDocument = "";
+        int iter = start;
+        var currentWord = "";
+        for (; iter < _document.Length; ++iter)
+        {
+            currentWord += _document[iter];
+            var cost = _dictionary.Contains(currentWord) ? 0 : currentWord.Length;
+            var subResult = ReSpace(iter + 1);
+            if (subResult.UnrecognisedChars + cost + 1 < bestUnrecognisedChars)
+            {
+                bestUnrecognisedChars = subResult.UnrecognisedChars + cost + 1;
+                bestDocument = currentWord + (" " + subResult.Document).TrimEnd();
+            }
+        }
+
+        _memory[start] = new(bestUnrecognisedChars, bestDocument);
+
+        return _memory[start]!;
+    }
+
+
+    public class ParseResult
+    {
+        public ParseResult(int unrecognisedChars, string document)
+        {
+            UnrecognisedChars = unrecognisedChars;
+            Document = document;
+        }
+
+        public string Document { get; set; }
+        public int UnrecognisedChars { get; set; }
+    }
+}
